ddaed3e09f6d41331bbb104d7e8b5327c5fc0ecd
Faeza Raziq <faezaraziqg@Gmail.com>
2025-03-16 13:28:09 +0700

    load environment variables from .env file if available

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,10 +12,16 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
+	// Load file .env, jika ada. Jika tidak, akan lanjut dengan env yang sudah di-set.
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file tidak ditemukan, melanjutkan dengan environment variable")
+	}
+
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		log.Fatal("DATABASE_URL belum di-set")
 	}
+
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
